7329cbfdb9267343ae09667aa6845bdadcdbc62b
Vhmit <viktor.9630@hotmail.com>
2024-01-27 20:09:19 -0300

    RemovePackages: Update list
    Change-Id: I9d97d5dbc66ed5bf3c9bfc73966b7d4971e2ecb4

--- RemovePackages/Android.mk
+++ RemovePackages/Android.mk
@@ -7,30 +7,26 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := \
     SoundAmplifierPrebuilt \
     ScribePrebuilt \
-    DevicePolicyPrebuilt \
     GoogleCamera \
     YouTube \
     GooglePrintRecommendationService \
     arcore \
-    TurboPrebuilt \
-    SafetyHubPrebuilt \
     LocationHistoryPrebuilt \
     TagGoogle \
     Music \
     Panic \
-    SafetyRegulatoryInfo \
-    Stk \
     DevCamera \
     Camera2 \
     arcore \
     RecorderPrebuilt \
     WellbeingPrebuilt \
-    WfcActivation \
+    GoogleTTS \
+    Talkback \
     Drive \
     PlayAutoInstallConfig \
     PrebuiltGmail \
     PrebuiltBugle \
-    AndroidAutoStubPrebuilt \
+    AndroidAutoStubPrebuilt
 
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
